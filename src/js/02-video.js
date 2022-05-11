525977c3580f7aadc8a3cdb2f7c5e825a7cc3691
OksanaOlefirenko <oksana_olefirenko@ukr.net>
2022-05-11 15:28:48 +0300

    update after check

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,24 +8,12 @@ player.on('timeupdate', throttle (onLocalStorageSave, 1000));
 function onLocalStorageSave (data) {
     // data is an object containing properties specific to that event
     // console.log(data);
-    const dataJSON = JSON.stringify(data);
+    const dataJSON = JSON.stringify(data.seconds);
     localStorage.setItem("videoplayer-current-time", dataJSON);
 }
 
-const savedTimeSeconds = JSON.parse (localStorage.getItem("videoplayer-current-time")).seconds;
+const savedTime = JSON.parse (localStorage.getItem("videoplayer-current-time"));
 
-player.setCurrentTime(savedTimeSeconds).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
-
-
+if (localStorage.getItem("videoplayer-current-time")) {
+    player.setCurrentTime(savedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,11 +2,7 @@ import throttle from "lodash.throttle";
 
 const refs = {
     form: document.querySelector(".feedback-form"),
-    email: document.querySelector(".feedback-form input"),
-    textarea: document.querySelector(".feedback-form textarea"),
 }
-
-const formData = {};
 
 refs.form.addEventListener("submit", onFormSubmit);
 refs.form.addEventListener("input", throttle(onFormInput, 500));
@@ -17,6 +13,7 @@ populateForm();
 function onFormInput(e) {
     // console.log(e.target.name);
     // console.log(e.target.value);
+    const formData = localStorage.getItem("feedback-form-state") ? JSON.parse(localStorage.getItem("feedback-form-state")) : {};
     formData[e.target.name] = e.target.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }
@@ -24,17 +21,17 @@ function onFormInput(e) {
 function populateForm() {
     const savedFormData = JSON.parse(localStorage.getItem("feedback-form-state"));
     if (savedFormData && savedFormData.email) {
-        refs.email.value = savedFormData.email;
+        refs.form.elements.email.value = savedFormData.email;
     }
     if (savedFormData && savedFormData.message) {
-        refs.textarea.value = savedFormData.message;
+        refs.form.elements.message.value = savedFormData.message;
     }
 }
 
 function onFormSubmit(e) {
     e.preventDefault();
-    e.target.reset();
-    console.log (JSON.parse(localStorage.getItem("feedback-form-state")));
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    refs.form.reset();
     localStorage.removeItem("feedback-form-state");
 }
 
